743c43d386e8592e06fb2af5217ae469a5d14f03
Nhu Gia Bach <nhugiabach@MacBook-Air-cua-Nhu.local>
2025-03-12 16:14:21 +1100

    BruteForce

--- src/main/java/Arrays_Hashing/ContainDuplicate.java
+++ src/main/java/Arrays_Hashing/ContainDuplicate.java
@@ -11,3 +11,33 @@
  * Input: nums = [1,2,3,4]
  * Output: false
  */
+
+package Arrays_Hashing;
+ 
+ 
+//This is a brute force approach with Time Complexity O(n^2) bc the nested loop and Space Complexity O(1)
+public class ContainDuplicate {
+    public static boolean IsDuplicate(int[] nums)
+        {
+            for(int i = 0; i < nums.length; i++ )
+            {
+                for(int j = i+1;j < nums.length;j++)
+                {
+                    if(nums[i]==nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        public static void main(String[] args) {
+            int[] test1 = {1,2,3,4};
+            int[] test2 = {1,2,3,3};
+            System.out.println(IsDuplicate(test1));
+            System.out.println(IsDuplicate(test2));        
+    }
+ }
+
+ 
