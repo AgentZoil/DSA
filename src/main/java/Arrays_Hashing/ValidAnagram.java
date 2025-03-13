d1746fb3bcb0fa793e1a329741d4eb422f5c1eeb
Nhu Gia Bach <nhugiabach@air-cua-nhu.lan.uow.edu.au>
2025-03-13 15:53:54 +1100

    Valid_Anagram

--- src/main/java/Arrays_Hashing/ValidAnagram.java
+++ src/main/java/Arrays_Hashing/ValidAnagram.java
@@ -18,3 +18,34 @@ Valid Anagram
  Constraints:
  s and t consist of lowercase English letters.
 */
+
+package Arrays_Hashing;
+public class ValidAnagram{
+    public static boolean isAnagram(String s,String t)
+    {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for(int val : count){
+            if(val !=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        String name1 = "bach";
+        String name2 = "halb";
+        System.out.println(isAnagram(name1, name2));
+    }
+}
